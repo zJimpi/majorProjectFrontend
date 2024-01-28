8acf536f7cbd5288bae7a04ee671f10b2ea48cac
thesangita <rupaidas498@gmail.com>
2024-01-28 20:23:22 +0530

    commit

--- src/app/admin/service/add-pkg.service.ts
+++ src/app/admin/service/add-pkg.service.ts
@@ -11,22 +11,22 @@ export class AddPkgService {
 
   // Method to add a destination by sending a POST request to the tomcat server.
   addPackage(data:any): Observable<any>{
-    return this._http.post('http://localhost:3000/package/savePackage',data)
+    return this._http.post('http://localhost:3000/package',data)
   }
 
   // Method to retrieve a list of destinations by sending a GET request to the tomcat server.
   getPackageList(): Observable<any> {
-    return this._http.get('http://localhost:3000/package/getPackageList');
+    return this._http.get('http://localhost:3000/package');
   }
 
   // Method to update a destination by sending a PUT request with the destination's ID and updated data.
   updatePackage(id: number, data: any): Observable<any> {
-    return this._http.put(`http://localhost:3000/package/updatePackage/${id}`, data);
+    return this._http.put(`http://localhost:3000/package/${id}`, data);
   }
 
   // Method to delete a destination by sending a DELETE request with the destination's ID.
   deletePackage(id: number): Observable<any> {
-    return this._http.delete(`http://localhost:3000/package/deletePackageById/${id}`);
+    return this._http.delete(`http://localhost:3000/package/${id}`);
   }
 
 }
